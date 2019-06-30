ddf8e767cb3df75b2292b46a407f5f1b97fa8977
Zhi Qu <zhiqu@stripe.com>
2019-06-30 13:53:24 -0700

    Shiji is done. Let me try some ElasticSearch

--- ruby/parse_shiji.rb
+++ ruby/parse_shiji.rb
@@ -1,18 +1,21 @@
 # frozen_string_literal: true
 
+require 'json'
+
+def generate_paragraph_json(source, chapter, text)
+  json_obj = {}
+  json_obj['source'] = source
+  json_obj['chapter'] = chapter
+  json_obj['text'] = text
+  json_obj
+end
+
 counter = 0
 chaptor = nil
-line_buffer = ''
-File.open('utf8/01史记.txt', 'r') do |f|
-  f.each_line do |line|
-    # skip first 4 lines
-    if counter < 4
-      counter += 1
-      next
-    elsif counter > 200
-      break
-    end
-
+line_buffer = StringIO.new
+json_array = []
+File.open('utf8/01史记.txt', 'r') do |utf8_input|
+  utf8_input.each_line do |line|
     # Check if it is a new chaptor
     new_chaptor = line[/●卷(.+)·(.+)第(.+)/, 2]
     if new_chaptor
@@ -22,12 +25,23 @@ File.open('utf8/01史记.txt', 'r') do |f|
       next
     end
 
-    # if line[/^    /]
-    #   # 
-    #   line = line.strip
-      
-    # end
-    # puts line
+    # 4 space before paragraph start
+    # puts "Processing line: #{line}"
+    if line[/^    /]
+      if line_buffer.size != 0 # rubocop:disable Style/ZeroLengthPredicate
+        text = line_buffer.string.clone
+        # puts "Dumping: #{text}"
+        json_array << generate_paragraph_json('史记', chaptor, text)
+        line_buffer.truncate(0)
+        line_buffer.rewind
+      end
+    end
+    line_buffer << line.strip
     counter += 1
+  end
+
+  # puts json_array
+  File.open('json/shiji.json', 'w') do |json_output|
+    json_output.write(JSON.pretty_generate(json_array))
   end
 end
